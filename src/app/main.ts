842be5b938e533ab7b577a802f74011de5225bf6
Kembert Nieves <kembert.nievesbriceno@externos-cl.cencosud.com>
2026-03-13 21:16:46 -0400

    add cors

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,11 +1,20 @@
 import "../config/env.ts";
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module.ts';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module.ts";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+    credentials: true,
+  });
+
   await app.listen(3000);
-  console.log('Application is running on: http://localhost:3000');
+  const appUrl = await app.getUrl();
+  const { host } = new URL(appUrl);
+  console.log(`Application is running on host: ${host}`);
 }
 bootstrap();
